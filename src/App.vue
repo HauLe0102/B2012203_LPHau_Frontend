c994780a1ef66a92ce7b6765fb13001a9b25dcc8
Hau Le 02 <haulp0210@gmail.com>
2024-10-31 11:10:42 +0700

    Khoi tao du an

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+//import AppHeader from "@/components/AppHeader.vue";
+export default {
+  //  components: {
+  //   AppHeader,
+  // },
+};
+</script>
+
+<template>
+  <h1>Hello, Vue.js!</h1>
+</template>
+
+<style>
+.page {
+  max-width: 400px;
+  margin: auto;
+}
+</style>
